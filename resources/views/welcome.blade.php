6796348a23bc4acd3cf2c55a77c391b1f195bb86
Eduardo <e.kuojeda@gmail.com>
2019-09-03 23:06:32 -0500

    7u7 <3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Te amo bb 7u7 </title>
+        <title>Laravel </title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Te amo bb 7u7 &#60;&#51;
                 </div>
 
                 <div class="links">
