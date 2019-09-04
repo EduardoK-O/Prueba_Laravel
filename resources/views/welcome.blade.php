9ee6125aa2c219062f6b324896ebd57602a3f250
Eduardo <e.kuojeda@gmail.com>
2019-09-03 23:13:24 -0500

    cambio etiqueta Laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    uwu
+                    Laravel
                 </div>
 
                 <div class="links">
