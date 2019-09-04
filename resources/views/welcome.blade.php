f92ec9a0879df2ce79c490a3a716cb61fe5ea712
Eduardo <e.kuojeda@gmail.com>
2019-09-03 23:12:06 -0500

    uwu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    uwu
                 </div>
 
                 <div class="links">
